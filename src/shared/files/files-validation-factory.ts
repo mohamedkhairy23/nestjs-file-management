7c400adf6078a6edfaecd7b48514713146eb94d6
Mohamed Khairy <mohamedkhairy17696@gmail.com>
2025-04-24 20:40:55 +0200

    refactoring (كلاكيت تاني مرة)

--- src/shared/files/files-validation-factory.ts
+++ src/shared/files/files-validation-factory.ts
@@ -1,5 +1,6 @@
 import {
   FileTypeValidator,
+  FileValidator,
   HttpStatus,
   MaxFileSizeValidator,
   ParseFilePipe,
@@ -7,26 +8,30 @@ import {
 } from '@nestjs/common';
 import { FileSignatureValidator } from './validators/file-signature.validator';
 
+const createFileValidators = (
+  maxSize: number,
+  fileType: RegExp | string,
+): FileValidator[] => [
+  // 1) Validate file size
+  new MaxFileSizeValidator({
+    maxSize: maxSize,
+    message: (maxSize) => `File is too big. Max file size is ${maxSize} bytes`,
+  }),
+  // 2) Validate file type (extensions)
+  new FileTypeValidator({
+    fileType: fileType,
+  }),
+
+  // 3) Custom validation
+  new FileSignatureValidator(),
+];
+
 export const createParseFilePipe = (
   maxSize: number,
-  fileType: RegExp,
+  fileType: RegExp | string,
 ): ParseFilePipe =>
   new ParseFilePipe({
-    validators: [
-      // 1) Validate file size
-      new MaxFileSizeValidator({
-        maxSize: maxSize,
-        message: (maxSize) =>
-          `File is too big. Max file size is ${maxSize} bytes`,
-      }),
-      // 2) Validate file type (extensions)
-      new FileTypeValidator({
-        fileType: fileType,
-      }),
-
-      // 3) Custom validation
-      new FileSignatureValidator(),
-    ],
+    validators: createFileValidators(maxSize, fileType),
     errorHttpStatusCode: HttpStatus.UNSUPPORTED_MEDIA_TYPE,
     exceptionFactory: (error: string) => {
       console.log('error', error);
