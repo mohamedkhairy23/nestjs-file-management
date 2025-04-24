c94a654afc3c7c3efbfd41be1a4012d9d4322a16
Mohamed Khairy <mohamedkhairy17696@gmail.com>
2025-04-24 21:49:10 +0200

    check if file type with a correct media type (refactoring (كلاكيت خامس مرة) )

--- src/shared/files/utils/file.util.ts
+++ src/shared/files/utils/file.util.ts
@@ -1,5 +1,12 @@
 import { FileType } from '../types/file.types';
+import { lookup } from 'mime-types';
 
 // ["png", "jpg"]]
-export const createFileTypeRegex = (fileTypes: FileType[]): RegExp =>
-  new RegExp(fileTypes.join('|'));
+export const createFileTypeRegex = (fileTypes: FileType[]): RegExp => {
+  // check if file type with a correct media type (In case of sending wrong mimetype will ignore it)
+  const mediaTypes = fileTypes
+    .map((type) => lookup(type))
+    .filter((type) => type !== false);
+
+  return new RegExp(mediaTypes.join('|'));
+};
